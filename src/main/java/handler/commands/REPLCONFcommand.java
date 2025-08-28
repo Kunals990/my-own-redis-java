4931cfcfdd002252bb052c01fa0cf6add523f011
Kunals990 <kunalsable9990@gmail.com>
2025-08-28 15:56:32 +0530

    ACKs with no commands-optimized

--- src/main/java/handler/commands/REPLCONFcommand.java
+++ src/main/java/handler/commands/REPLCONFcommand.java
@@ -15,7 +15,8 @@ public class REPLCONFcommand implements Command {
 
         if(replicationInfo.getRole().equalsIgnoreCase("slave")){
             if(commandContext.args.get(1).equalsIgnoreCase("GETACK")){
-                return "*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n"+replicationInfo.getReplOffset()+"\r\n";
+                String offsetStr = String.valueOf(replicationInfo.getReplOffset());
+                return "*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$" + offsetStr.length() + "\r\n" + offsetStr + "\r\n";
             }
             else{
                 return null;
