2e434bd34bcc8b9033f3ac32b0e9078fca85fe51
Kunals990 <kunalsable9990@gmail.com>
2025-08-28 16:27:02 +0530

    ACKs with commands-fixed ping command

--- src/main/java/handler/commands/PINGcommand.java
+++ src/main/java/handler/commands/PINGcommand.java
@@ -1,5 +1,6 @@
 package handler.commands;
 
+import config.ReplicationInfo;
 import handler.Command;
 import handler.CommandContext;
 
@@ -7,6 +8,9 @@ public class PINGcommand implements Command {
 
     @Override
     public String execute(CommandContext commandContext) {
+        if(ReplicationInfo.getInstance().getRole().equalsIgnoreCase("slave")){
+            return null;
+        }
         return "+PONG\r\n";
     }
 }
