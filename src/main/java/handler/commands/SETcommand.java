c136913be5fd99886e469ff3f0d3c595deb18ef4
Kunals990 <kunalsable9990@gmail.com>
2025-08-28 22:30:38 +0530

    Read values with expiry-fixed set method

--- src/main/java/handler/commands/SETcommand.java
+++ src/main/java/handler/commands/SETcommand.java
@@ -15,11 +15,12 @@ public class SETcommand implements Command {
     public String execute(CommandContext commandContext) throws IOException {
         if (commandContext.args.size() < 3) return "-ERR wrong number of arguments for 'set'\r\n";
 
-        int expiry = -1; // -1 means no expiry
+        long expiryMillis = -1; // -1 means no expiry
 
         if (commandContext.args.size() == 5 && commandContext.args.get(3).equalsIgnoreCase("PX")) {
             try {
-                expiry = Integer.parseInt(commandContext.args.get(4));
+                long duration = Long.parseLong(commandContext.args.get(4));
+                expiryMillis = System.currentTimeMillis() + duration;
             } catch (NumberFormatException e) {
                 return "-ERR PX value is not a valid integer\r\n";
             }
@@ -28,7 +29,7 @@ public class SETcommand implements Command {
         String key = commandContext.args.get(1);
         String value = commandContext.args.get(2);
 
-        store.set(key,value,expiry);
+        store.set(key,value,expiryMillis);
 
         if(ReplicationInfo.getInstance().getRole().equalsIgnoreCase("slave")){
             return null;
