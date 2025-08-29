5b9c5e3a93849ccd8cd420b5df2017c4bbf7988b
Kunals990 <kunalsable9990@gmail.com>
2025-08-29 07:17:44 +0530

    PING in subscribed mode #lf1

--- src/main/java/handler/commands/server/PINGcommand.java
+++ src/main/java/handler/commands/server/PINGcommand.java
@@ -1,6 +1,7 @@
 package handler.commands.server;
 
 import config.ReplicationInfo;
+import handler.ClientState;
 import handler.Command;
 import handler.CommandContext;
 
@@ -8,9 +9,13 @@ public class PINGcommand implements Command {
 
     @Override
     public String execute(CommandContext commandContext) {
-        if(ReplicationInfo.getInstance().getRole().equalsIgnoreCase("slave")){
+        ClientState clientState = commandContext.clientState;
+        if(clientState!=null && clientState.inSubscribedMode){
+            return "*2\r\n$4\r\npong\r\n$0\r\n\r\n";
+        }else if(ReplicationInfo.getInstance().getRole().equalsIgnoreCase("slave")){
             return null;
         }
+
         return "+PONG\r\n";
     }
 }
