a92ab14f486091aab3805dc51742d26be7f932b7
Kunals990 <kunalsable9990@gmail.com>
2025-08-31 12:20:59 +0530

    Decode coordinates #hb5-fixed

--- src/main/java/utils/GeoHash.java
+++ src/main/java/utils/GeoHash.java
@@ -25,12 +25,14 @@ public class GeoHash {
     }
 
     public static long encode(double latitude, double longitude) {
-        double normalizedLatitude = Math.pow(2, 26) * (latitude - MIN_LATITUDE) / LATITUDE_RANGE;
-        double normalizedLongitude = Math.pow(2, 26) * (longitude - MIN_LONGITUDE) / LONGITUDE_RANGE;
-        int latInt = (int) normalizedLatitude;
-        int lonInt = (int) normalizedLongitude;
+        double latFactor = Math.pow(2, 26) / LATITUDE_RANGE;
+        double lonFactor = Math.pow(2, 26) / LONGITUDE_RANGE;
+        int latInt = (int)((latitude - MIN_LATITUDE) * latFactor);
+        int lonInt = (int)((longitude - MIN_LONGITUDE) * lonFactor);
+
         return interleave(latInt, lonInt);
     }
+
     public static class Coordinates {
         public final double latitude;
         public final double longitude;
