27f86be053f21df73d1d93cb1f5bca77ade87811
Kunals990 <kunalsable9990@gmail.com>
2025-08-31 11:25:10 +0530

    Validate coordinates #ck3

--- src/main/java/handler/commands/geospatial/GEOADDcommand.java
+++ src/main/java/handler/commands/geospatial/GEOADDcommand.java
@@ -6,11 +6,34 @@ import handler.CommandContext;
 import java.io.IOException;
 
 public class GEOADDcommand implements Command {
+
+    private static final double MIN_LONGITUDE = -180.0;
+    private static final double MAX_LONGITUDE = 180.0;
+    private static final double MIN_LATITUDE = -85.05112878;
+    private static final double MAX_LATITUDE = 85.05112878;
+
     @Override
     public String execute(CommandContext commandContext) throws IOException {
         if (commandContext.args.size() != 5) {
             return "-ERR wrong number of arguments for 'geoadd' command\r\n";
         }
+
+        double longitude;
+        double latitude;
+
+        try {
+            longitude = Double.parseDouble(commandContext.args.get(2));
+            latitude = Double.parseDouble(commandContext.args.get(3));
+        } catch (NumberFormatException e) {
+            return "-ERR value is not a valid float\r\n";
+        }
+        if (longitude < MIN_LONGITUDE || longitude > MAX_LONGITUDE) {
+            return "-ERR invalid longitude argument\r\n";
+        }
+        if (latitude < MIN_LATITUDE || latitude > MAX_LATITUDE) {
+            return "-ERR invalid latitude argument\r\n";
+        }
+
         return ":1\r\n";
     }
 }
